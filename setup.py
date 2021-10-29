7c91accaf8dc97c008f4b9d4e2c8cd86210627c2
noamteyssier <nb.teyssier@gmail.com>
2021-10-29 12:27:12 -0700

    added required dependencies to setup

--- setup.py
+++ setup.py
@@ -7,5 +7,11 @@ setup(
     author_email="Noam.Teyssier@ucsf.edu",
     packages=["geomux"],
     description="a tool to assign an identity to a table of barcode guides",
-    entry_points={'console_scripts': ['geomux = geomux.__main__:main_cli']}
+    entry_points={'console_scripts': ['geomux = geomux.__main__:main_cli']},
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "plotly",
+        "umap-learn"]
 )
