60593f19fb9bd8631062fef9f0d9cea2ca78f1c4
noamteyssier <nb.teyssier@gmail.com>
2021-10-29 10:49:43 -0700

    module setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="geomux",
+    version="0.1.0",
+    author="Noam Teysser",
+    author_email="Noam.Teyssier@ucsf.edu",
+    packages=["geomux"],
+    description="a tool to assign an identity to a table of barcode guides",
+    entry_points={'console_scripts': ['geomux = geomux.__main__:main_cli']}
+)
