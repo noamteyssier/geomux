e3e63d76a219102bdfcd922616befd2528c4d48c
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-01-09 18:02:18 -0800

    breaking changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="geomux",
-    version="0.1.0",
+    version="0.2.0",
     author="Noam Teysser",
     author_email="Noam.Teyssier@ucsf.edu",
     packages=["geomux"],
