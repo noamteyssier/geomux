43ef93c87a429e50c4eb55d5f56c9a2ebec06cd5
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-01-24 14:58:07 -0800

    added convenience function to load anndata to table of expected format

--- geomux/utils.py
+++ geomux/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import anndata as ad
 
 
 def read_table(filename: str, sep: str = "\t") -> pd.DataFrame:
@@ -12,4 +13,20 @@ def read_table(filename: str, sep: str = "\t") -> pd.DataFrame:
             names=["barcode", "guide", "n_umi"],
             dtype={"barcode": str, "guide": str, "n_umi": int},
             sep=sep)
+    return frame
+
+
+def read_anndata(filename: str) -> pd.DataFrame:
+    """
+    Reads an .h5ad formatted file and
+    confirms that the file is in an expected format
+    """
+    frame = ad.read(filename).to_df().reset_index()
+    frame = frame\
+        .melt(
+            id_vars=frame.columns.values[0],
+            var_name="guide",
+            value_name="n_umi")
+    frame = frame[frame.n_umi > 0]
+    frame.columns = ["barcode", "guide", "n_umi"]
     return frame
