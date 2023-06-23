ba451ace216a7042773af68dd8ebf755bc30c908
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-06-23 10:50:28 -0700

    update testing and gitignore

--- tests/test_geomux.py
+++ tests/test_geomux.py
@@ -9,14 +9,7 @@ def sample_barcode(b_size):
     """
     return "".join(np.random.choice(["A", "C", "T", "G"], size=b_size))
 
-
-def test_geomux():
-    """
-    tests basic usage
-    """
-    n = 1000
-    b_size = 20
-    g_size = 4
+def generate_matrix(n, b_size, g_size):
     frame = pd.DataFrame(
         {
             "barcode": [sample_barcode(b_size) for _ in np.arange(n)],
@@ -24,7 +17,19 @@ def test_geomux():
             "n_umi": [np.random.choice(100) for _ in np.arange(n)],
         }
     )
-    geom = Geomux()
-    geom.fit(frame)
-    geom.predict()
-    geom.assignments()
+    matrix = frame.pivot_table(index="barcode", columns="guide", values="n_umi").fillna(0)
+    return matrix
+
+
+def test_geomux():
+    """
+    tests basic usage
+    """
+    n = 100
+    b_size = 10
+    g_size = 4
+    matrix = generate_matrix(n, b_size, g_size)
+    gx = Geomux(matrix)
+    gx.test()
+    assignments = gx.assignments()
+    assert assignments.shape[0] == n
