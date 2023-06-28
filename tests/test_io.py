5f03fdf55001274304cdb1d55191e5680d80c0d9
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-06-28 11:17:16 -0700

    added example test for sparse

--- tests/test_io.py
+++ tests/test_io.py
@@ -14,6 +14,19 @@ def test_anndata_example():
     assert assignments.shape[0] == matrix.shape[0]
 
 
+def test_anndata_sparse_csr_example():
+    """
+    loads an existing anndata and processes it
+    """
+    matrix = read_anndata("example/example_sparse.h5ad")
+    gx = Geomux(
+        matrix, cell_names=matrix.index.values, guide_names=matrix.columns.values
+    )
+    gx.test()
+    assignments = gx.assignments()
+    assert assignments.shape[0] == matrix.shape[0]
+
+
 def test_table_example():
     """
     loads an existing table and processes it
