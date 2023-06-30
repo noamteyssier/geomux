47bd81ded7b5e5bed0616cf9ec41f9729222a866
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-06-30 16:50:30 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="geomux",
-    version="0.2.3",
+    version="0.2.4",
     author="Noam Teysser",
     author_email="Noam.Teyssier@ucsf.edu",
     packages=["geomux"],
