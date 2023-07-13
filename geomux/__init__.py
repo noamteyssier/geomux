844b7592546c97c68e4cb318ba65fef320e82972
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-07-12 17:02:52 -0700

    black formatting

--- geomux/__init__.py
+++ geomux/__init__.py
@@ -1,6 +1,7 @@
 from .geomux import Geomux
 from .utils import read_table, read_anndata
 import logging
+
 logging.basicConfig(
     format="%(asctime)s :: %(levelname)s :: %(message)s",
     level=logging.INFO,
