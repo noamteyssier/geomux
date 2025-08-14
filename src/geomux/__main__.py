3a7dace303bc9f604e27e6e415a38fe4493c9cba
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-08-14 13:21:18 -0700

    refactor(cli): improve cli with typer

--- src/geomux/__main__.py
+++ src/geomux/__main__.py
@@ -1,8 +1,9 @@
 import argparse
-import sys
 
 import anndata as ad
 import numpy as np
+import typer
+from typing_extensions import Annotated
 
 from geomux import Geomux, read_table
 
@@ -71,37 +72,70 @@ def get_args():
     return args
 
 
-def main_cli():
-    args = get_args()
+def main_cli(
+    input: Annotated[
+        str, typer.Argument(help="Input file path (tsv/h5ad) to assign guides.")
+    ],
+    output: Annotated[
+        str, typer.Argument(help="Output file path (tsv) to save assignments.")
+    ] = "geomux.tsv",
+    min_umi: Annotated[
+        int, typer.Option(help="Minimum UMI count to consider a barcode")
+    ] = 5,
+    min_cells: Annotated[
+        int,
+        typer.Option(help="Minimum number of barcodes to consider a guide"),
+    ] = 100,
+    pvalue_threshold: Annotated[
+        float, typer.Option(help="Maximum pvalue (fdr) to consider a guide-assignment")
+    ] = 0.05,
+    lor_threshold: Annotated[
+        float,
+        typer.Option(
+            help="Log odds ratio threshold to use (default=10.0)",
+        ),
+    ] = 10.0,
+    correction: Annotated[
+        str, typer.Option(help="Pvalue correction method to use (default=bh)")
+    ] = "bh",
+    n_jobs: Annotated[
+        int,
+        typer.Option(
+            help="Number of jobs to use when calculating hypergeometric distributions (default=1)"
+        ),
+    ] = 1,
+):
+    # args = get_args()
 
-    if args.input.endswith(".h5ad"):
-        matrix = ad.read_h5ad(args.input)
+    if input.endswith(".h5ad"):
+        matrix = ad.read_h5ad(input)
         cell_names = np.array(matrix.obs_names.values)
         guide_names = np.array(matrix.var_names.values)
     else:
-        matrix = read_table(args.input)
+        matrix = read_table(input)
         cell_names = np.array(matrix.index.values)
         guide_names = np.array(matrix.columns.values)
 
-    if args.correction not in ["bh", "bonferroni", "by"]:
+    if correction not in ["bh", "bonferroni", "by"]:
         raise ValueError("Correction method must be one of: bh, bonferroni, by")
 
     gx = Geomux(
         matrix,
         cell_names=cell_names,
         guide_names=guide_names,
-        min_umi=args.min_umi,
-        min_cells=args.min_cells,
-        n_jobs=args.n_jobs,
-        method=args.correction,
+        min_umi=min_umi,
+        min_cells=min_cells,
+        n_jobs=n_jobs,
+        method=correction,
     )
     gx.test()
     assignments = gx.assignments(
-        pvalue_threshold=args.pvalue_threshold,
-        lor_threshold=args.lor_threshold,
+        pvalue_threshold=pvalue_threshold,
+        lor_threshold=lor_threshold,
     )
 
-    if not args.output:
-        assignments.to_csv(sys.stdout, sep="\t", index=False)
-    else:
-        assignments.to_csv(args.output, sep="\t", index=False)
+    assignments.to_csv(output, sep="\t", index=False)
+
+
+def main():
+    typer.run(main_cli)
