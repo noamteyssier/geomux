2190ab32de3e7bad004d2bddb400ed0f4b6ebac9
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-08-14 13:21:41 -0700

    chore(cli): remove old argparse

--- src/geomux/__main__.py
+++ src/geomux/__main__.py
@@ -1,75 +1,9 @@
-import argparse
-
 import anndata as ad
 import numpy as np
 import typer
 from typing_extensions import Annotated
 
 from geomux import Geomux, read_table
-
-
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-i", "--input", type=str, required=True, help="Input table to assign"
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        type=str,
-        required=False,
-        help="output table of barcode assignments (default=stdout)",
-    )
-    parser.add_argument(
-        "-u",
-        "--min_umi",
-        type=int,
-        required=False,
-        default=5,
-        help="minimum number of UMIs to consider a cell (default=5)",
-    )
-    parser.add_argument(
-        "-c",
-        "--min_cells",
-        type=int,
-        required=False,
-        default=100,
-        help="minimum number of cells to consider a guide (default=100)",
-    )
-    parser.add_argument(
-        "-t",
-        "--pvalue_threshold",
-        type=float,
-        required=False,
-        default=0.05,
-        help="Pvalue threshold to use after pvalue correction (default=0.05)",
-    )
-    parser.add_argument(
-        "-T",
-        "--lor_threshold",
-        type=float,
-        required=False,
-        default=10.0,
-        help="Log odds ratio threshold to use (default=10.0)",
-    )
-    parser.add_argument(
-        "-C",
-        "--correction",
-        type=str,
-        required=False,
-        default="bh",
-        help="Pvalue correction method to use (default=bh)",
-    )
-    parser.add_argument(
-        "-j",
-        "--n_jobs",
-        type=int,
-        required=False,
-        default=1,
-        help="Number of jobs to use when calculating hypergeometric distributions (default=1)",
-    )
-    args = parser.parse_args()
-    return args
 
 
 def main_cli(
