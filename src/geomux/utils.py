9a0e2aeb4415ca32f9d838ac73aa8f406ce8fec8
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-09-26 12:47:05 -0700

    refactor: make sparse path the only path

--- src/geomux/utils.py
+++ src/geomux/utils.py
@@ -1,24 +1,5 @@
 import numpy as np
-import pandas as pd
 import polars as pl
-
-
-def read_table(filename: str, sep: str = "\t") -> pd.DataFrame:
-    """
-    Reads an input file and confirms that
-    the file is in an expected format
-    """
-    frame = pd.read_csv(
-        filename,
-        header=None,
-        names=["barcode", "guide", "n_umi"],
-        dtype={"barcode": str, "guide": str, "n_umi": int},
-        sep=sep,
-    )
-    matrix = frame.pivot_table(
-        index="barcode", columns="guide", values="n_umi", fill_value=0
-    )
-    return matrix
 
 
 def assignment_statistics(assignments: pl.DataFrame) -> dict:
