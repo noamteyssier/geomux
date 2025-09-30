f53bb8f98568d3b8777563d583c45d7d945a38a1
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-09-30 15:25:22 -0700

    feat: added mixture model option to cli

--- src/geomux/__main__.py
+++ src/geomux/__main__.py
@@ -4,6 +4,8 @@ import anndata as ad
 import typer
 from typing_extensions import Annotated
 
+from geomux.mixture import gaussian_mixture
+
 from . import geomux
 from .utils import assignment_statistics
 
@@ -44,23 +46,43 @@ def main_cli(
         str | None,
         typer.Option(help="Output file to write assignment statistics to as json"),
     ] = None,
+    method: Annotated[
+        str, typer.Option(help="Method to use for assignment (geomux/mixture)")
+    ] = "geomux",
+    n_jobs: Annotated[
+        int,
+        typer.Option(
+            help="Number of jobs to use for parallel processing (mixture model only). -1 for all available cores."
+        ),
+    ] = -1,
 ):
     adata = ad.read_h5ad(input)
-    results = geomux(
-        adata,
-        min_umi_cells=min_umi_cells,
-        min_umi_guides=min_umi_guides,
-        fdr_threshold=fdr_threshold,
-        lor_threshold=lor_threshold,
-        adaptive_lor_scalar=adaptive_lor_scalar,
-        subtract=subtract,
-    )
-    results.write_csv(output, separator="\t")
 
-    if stats:
-        statistics = assignment_statistics(results)
-        with open(stats, "w+") as f:
-            f.write(json.dumps(statistics, indent=2))
+    if method == "geomux":
+        results = geomux(
+            adata,
+            min_umi_cells=min_umi_cells,
+            min_umi_guides=min_umi_guides,
+            fdr_threshold=fdr_threshold,
+            lor_threshold=lor_threshold,
+            adaptive_lor_scalar=adaptive_lor_scalar,
+            subtract=subtract,
+        )
+        results.write_csv(output, separator="\t")
+
+        if stats:
+            statistics = assignment_statistics(results)
+            with open(stats, "w+") as f:
+                f.write(json.dumps(statistics, indent=2))
+    elif method == "mixture":
+        results = gaussian_mixture(
+            adata,
+            min_umi_cells=min_umi_cells,
+            n_jobs=n_jobs,
+        )
+        results.write_csv(output, separator="\t")
+    else:
+        raise ValueError(f"Invalid method: {method}. Use `geomux` or `mixture`")
 
 
 def _version():
